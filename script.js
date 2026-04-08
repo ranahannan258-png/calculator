a1c66b643bd4e5ba04502c7823e66a56f35ff9a3
Muhammad Hannan <ranahannan258@gmail.com>
2026-04-08 03:50:22 -0700

    JS for calculator

---
+++ script.js
@@ -0,0 +1,73 @@
+
+let arr = [];
+let display = document.querySelector(".display");
+let calculate = document.querySelector(".calculate");
+let delete1 = document.querySelector(".delete");
+let delete_all = document.querySelector(".alldelete");
+let buttons = document.querySelectorAll(".btn");
+let operation_btn = document.querySelectorAll(".opbutton");
+let lastvalue = '';
+
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let pressbtn = e.target.dataset.value;
+        if(!pressbtn)
+            return 
+        // 1. prevent starting with operator
+
+            if (arr.length === 0 && ["+", "-", "*", "/"].includes(pressbtn)) {
+    return;
+ }
+
+        // 2. prevent double operators
+        if (
+            ["+", "-", "*", "/"].includes(lastvalue) &&
+            ["+", "-", "*", "/"].includes(pressbtn)
+        ) {
+            return;
+        }
+
+        // 3. prevent multiple decimals
+        if (pressbtn === ".") {
+            let lastNumber = arr.join('').split(/[\+\-\*\/]/).pop();
+            if (lastNumber.includes(".")) return;
+        }
+
+        arr.push(pressbtn);
+        display.innerHTML = arr.join('');
+        lastvalue = pressbtn;
+    });
+});
+
+
+delete1.addEventListener("click", () => {
+    arr.pop();
+    console.log(arr)
+    display.innerHTML = arr.join('');
+})
+
+delete_all.addEventListener("click", () => {
+    arr.splice(0)
+    display.innerHTML = arr;
+
+})
+
+calculate.addEventListener("click", () => {
+    try {
+        let result = eval(arr.join(''))
+        console.log(result)
+        display.innerHTML = result;
+        arr = [result]
+    }
+    catch {
+        display.innerHTML = 'Error'
+        setTimeout(() => {
+            display.innerHTML = ''
+        }, 1000);
+        arr = []
+    }
+
+})
+
+
